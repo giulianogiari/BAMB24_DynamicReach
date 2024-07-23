efde0d523de4f119cd3bd5428367ccfa7756ff91
jlageman <78727276+jlageman@users.noreply.github.com>
2024-07-23 10:35:26 +0200

    Update agents.py

--- agents.py
+++ agents.py
@@ -9,10 +9,10 @@ class RandomAgent:
 
   def act(self, state):
     # determine velocity based on time
-    if state['time']<260:
+    if state['time']<0:
       velocity = 0
     else:
-      velocity = 1
+      velocity = self.action_space['velocity'].sample()[0]
 
     # pick random angle
     angle = self.action_space['angle'].sample()[0]
@@ -22,8 +22,9 @@ class RandomAgent:
     return action
 
 # OPTIMAL WITH MOTOR NOISE #
-class OptimalNoisyAgent:
+class OptimalAgent:
   def __init__(self, env, motor_noise=1):
+    self.env = env
     self.action_space = env.action_space
     self.observation_space = env.observation_space
     self.locationX = env.locationX
@@ -33,13 +34,67 @@ class OptimalNoisyAgent:
 
   def act(self, state):
     # determine velocity based on time
-    if state['time']<260:
+    if state['time']<0:
+      angle = 0
+      velocity = 0
+      action = {'angle': angle, 'velocity': velocity}
+      return action
+    
+    else:
+      # choose angle and velocity to minimize cost
+      possible_angles = np.linspace(-np.pi, np.pi, 360)
+      possible_velocities = [0,1,2,3]
+      min_cost, best_action = 999999, None
+      for angle in possible_angles:
+        for velocity in possible_velocities:
+          action = {'angle': angle, 'velocity': velocity}
+          _, cost, _ = self.env.simulate_step(state, action)
+          if cost < min_cost:
+            min_cost = cost
+            best_action = action
+      
+      # apply some motor noise and return
+      best_action['angle'] = best_action['angle'] + np.random.normal(0, self.motor_noise)
+
+      return best_action
+
+# OPTIMAL WITH BELIEF UPDATING # NOT FINISHED
+class OptimalBeliefAgent:
+  def __init__(self, env, lr=0.02, belief_noise=0.01, motor_noise=1):
+    self.action_space = env.action_space
+    self.observation_space = env.observation_space
+    self.locationX = env.locationX
+    self.locationY = env.locationY
+    # implement parameters
+    self.lr = lr
+    self.belief_noise = belief_noise
+    self.motor_noise = motor_noise
+    # initialize belief about target
+    self.p = 0.5
+
+  def act(self, state):
+    self.target = env.target
+    self.jump_time = env.jump_time
+    self.postjump_target = env.postjump_target
+    # start with belief updating step
+    if self.target!=self.postjump_target and state['time']>self.jump_time:
+        self.p -= self.lr+np.random.normal(0, self.belief_noise)
+    else:
+        self.p += self.lr+np.random.normal(0, self.belief_noise)
+        
+    # determine velocity based on time
+    if state['time']<0:
       velocity = 0
     else:
       velocity = 1
-
-    # angle that minimizes distance to target + small amount of gaussian noise
-    tloc_x, tloc_y = (self.locationX[state['target']], self.locationY[state['target']])
+    
+    # use belief to figure out the goal target location
+    if np.random.rand() < self.p:
+        tloc_x, tloc_y = (self.locationX[self.target], self.locationY[self.target])
+    else:
+        tloc_x, tloc_y = (self.locationX[self.postjump_target], self.locationY[self.postjump_target])
+    
+    # angle that minimizes distance to believed target + small amount of gaussian noise
     x,y = state['position']
     possible_actions = np.linspace(-np.pi, np.pi, 360)
     dist = [np.sqrt(((x+1000/130*np.cos(ori))-tloc_x)**2 + ((y+1000/130*np.sin(ori))-tloc_y)**2) for ori in possible_actions]
@@ -48,36 +103,3 @@ class OptimalNoisyAgent:
     # assemble and return action
     action = {'angle': angle, 'velocity': velocity}
     return action
-
-# OPTIMAL WITH MOTOR NOISE, SMOOTHER TRAJECTORY #
-class OptimalSmoothNoisyAgent:
-  def __init__(self, env, motor_noise=1):
-    self.action_space = env.action_space
-    self.observation_space = env.observation_space
-    self.locationX = env.locationX
-    self.locationY = env.locationY
-    # implement parameters
-    self.motor_noise = motor_noise
-    # initialize angle
-    self.angle = 0
-
-  def act(self, state):
-    # determine velocity based on time
-    if state['time']<260:
-      velocity = 0
-    else:
-      velocity = 1
-
-    # angle that minimizes distance to target + small amount of gaussian noise
-    tloc_x, tloc_y = (self.locationX[state['target']], self.locationY[state['target']])
-    x,y = state['position']
-    possible_actions = np.linspace(self.angle-np.pi/4, self.angle+np.pi/4, 360)
-    dist = [np.sqrt(((x+1000/130*np.cos(ori))-tloc_x)**2 + ((y+1000/130*np.sin(ori))-tloc_y)**2) for ori in possible_actions]
-    angle = possible_actions[dist.index(min(dist))] + np.random.normal(0, self.motor_noise)
-    self.angle = angle
-
-    # assemble and return action
-    action = {'angle': angle, 'velocity': velocity}
-    return action
-
-
