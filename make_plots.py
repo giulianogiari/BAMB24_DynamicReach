7de323bc5d99973524eabe5b0c55797a9c9d6ebb
jlageman <78727276+jlageman@users.noreply.github.com>
2024-07-23 17:35:10 +0200

    add simulate_data

--- make_plots.py
+++ make_plots.py
@@ -1,25 +1,36 @@
 ## BASIC PLOT OF TASK ENVIRONMENT ##
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import splprep, splev
 
-def get_traj_list(env, agent, target):
+def simulate_data(env, agent, N, target=None, postjump_target=None):
   traj_list = []
-  for _ in range(10):
-    state, terminated = env.reset(set_target=target)
+  belief_list = []
+  target_list = []
+  for _ in range(N):
+    agent.reset_belief()
+    state, terminated = env.reset(set_target=target, set_postjump_target=postjump_target)
     trajectory = {'x': [], 'y': []}
+    belief = []
+    tg = []
     while not terminated:
       action = agent.act(state)
       next_state, reward, terminated = env.step(action)
+      belief.append(agent.p)
+      tg.append(next_state['target'])
       if next_state['time'] > 0:
         x, y = next_state['position']
         trajectory['x'].append(x)
         trajectory['y'].append(y)
       state = next_state
     traj_list.append(trajectory)
-  return traj_list
+    belief_list.append(belief)
+    target_list.append(tg)
 
-def plot_traj(env, agent, target):
-    traj_list = get_traj_list(env, agent, target)
+  return traj_list, belief_list, target_list
+
+def plot_traj(env, agent, N, target=None, postjump_target=None, smooth=True):
+    traj_list, _, _ = simulate_data(env, agent, N, target, postjump_target)
     # set up figure
     plt.figure(figsize=(3,3), dpi=200)
     plt.plot(0,0,'o', color='black',markersize=2)
@@ -32,6 +43,8 @@ def plot_traj(env, agent, target):
     for i in range(8):
       if i==target:
         color='red'
+      elif i==postjump_target:
+        color='blue'
       else:
         color='gray'
       x = 80*np.sin([0*np.pi/180, 45*np.pi/180, 90*np.pi/180, 135*np.pi/180, 180*np.pi/180, 225*np.pi/180, 270*np.pi/180, 315*np.pi/180][i])
